c25cd88111729a416a7980607531a516e8861563
lmelker <lasse@melker.org>
2021-02-25 11:22:56 +0100

    Add env, for eazy change of main settings

--- reset.sh
+++ reset.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+docker-compose down
 rm -R -f ./pgdata
 rm -R -f ./jira
 mkdir ./jira
 chown -R 2001:2001 ./jira
+
+docker volume rm jira-datacenter-sandbox-environment_jira-node1-volume
+docker volume rm jira-datacenter-sandbox-environment_jira-node2-volume
+docker volume rm jira-datacenter-sandbox-environment_jira-node3-volume
 
